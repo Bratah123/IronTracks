d79711014425d5e20f565f1fa6ddca619c6525e7
Bratah123 <brandonnguyen3301@gmail.com>
2023-11-04 13:18:58 -0700

    FEAT: Init files

---
+++ PTCGLDeckTracker/IronTracks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Harmony;
+using HarmonyLib;
+using MelonLoader;
+
+namespace PTCGLDeckTracker
+{
+    public class IronTracks : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                LoggerInstance.Msg("User Clicked F");
+            }
+        }
+
+        public override void OnGUI()
+        {
+            var width = 250;
+            var height = 300;
+            GUI.Box(new Rect(Screen.width - width, 0, width, height), "Deck");
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(MatchManager), "SendMatchStartTelemetry")]
+        class Patch
+        {
+            static void Prefix(MatchManager __instance, NetworkMatchController.MatchDetails game)
+            {
+                var playerOneName = game.players[0].playerName;
+                var playerTwoName = game.players[1].playerName;
+                Melon<IronTracks>.Logger.Msg(playerOneName + " vs. " + playerTwoName);
+            }
+        }
+    }
+
+}
---
+++ PTCGLDeckTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using PTCGLDeckTracker; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(IronTracks), "Iron Tracks", "0.0.1", "Bratah")]
+[assembly: MelonGame("pokemon", "Pokemon TCG Live")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PTCGLDeckTracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PTCGLDeckTracker")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39907106-d171-4af3-9f85-053db6a4f731")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
